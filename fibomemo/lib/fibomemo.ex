67a525932f1f886237075a5dad6f2fe91cea52e9
Mathieu Scolas <worming@hotmail.be>
2023-01-25 17:16:08 +0100

    use logger

--- fibomemo/lib/fibomemo.ex
+++ fibomemo/lib/fibomemo.ex
@@ -1,13 +1,16 @@
 defmodule Fibomemo do
-  def fib (0) do
-    1
-  end
-  
-  def fib (1) do
+  require Logger
+
+  def fib(0) do
     1
   end
 
-  def fib (n) do
-    fib(n-1) + fib(n-2)
+  def fib(1) do
+    1
+  end
+
+  def fib(n) do
+    Logger.info("fib for #{n}")
+    fib(n - 1) + fib(n - 2)
   end
 end
